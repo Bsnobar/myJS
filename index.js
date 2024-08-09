9cd1c072791e787befbca633f737cb289231aa9a
Batool <batoolsnobar0@gmail.com>
2024-08-09 20:15:34 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function boxcheker() {
+    var textBoxValue = document.getElementById('box').value;
+    var textLength  = textBoxValue.length;
+    
+    if (textBoxLength === 0 ) {
+        alert("empty box, enter smthn");
+    }    else {
+        alert("thank you");
+        }
+    }
